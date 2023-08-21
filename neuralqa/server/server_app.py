fa3bf0896b5835b44597a6b018d2eb2ff0627ad8
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-21 09:50:20 +0100

    Instalation adaptation - round edges

--- neuralqa/server/server_app.py
+++ neuralqa/server/server_app.py
@@ -4,7 +4,7 @@ import os
 
 
 def launch_server(host="127.0.0.1", port=5000, workers=1, reload=False):
-    uvicorn.run("neuralqa.server.serve:app", host=host, port=port, workers=workers,
+    uvicorn.run("server.serve:app", host=host, port=port, workers=workers,
                 log_level="info", reload=reload)
 
 
